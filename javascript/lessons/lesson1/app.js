d07f305491dac969657be932f104de14a7d9e0cf
MbuguaDennis <dmbugua.developer@gmail.com>
2025-02-19 00:16:23 +0300

    Add: Add lesson files

---
+++ javascript/lessons/lesson1/app.js
@@ -0,0 +1,60 @@
+// This is an inline comment
+/* multi-line comment
+*/
+// declare variables using keywords
+// var,let,const
+// Boolean
+let isLoggedIn = true;
+let stephIsAsleep = false;
+let hasBooked = true;
+let stephIsTall = false;
+console.log(stephIsAsleep)
+
+//Undefined
+let firstName;
+// let allows reassignment
+firstName = "Dennistephanie";
+// Null
+let myName =null;
+let stephIsEating = null;
+// Numbers
+let age = 50;
+let results  = 90.4;
+let temperature = -30;
+
+// Strings
+const LASTNAME = "Mbugua";
+let greetings = "HelloWorld";
+
+// Objects /key-value
+
+let person = {
+    age: 50,
+    hobby:"sleep",
+    profession: "Developer"
+}
+
+// arrays
+let items = [50,"Stephanie",person = {
+    age: 50,
+    hobby:"sleep",
+    profession: "Developer"
+},true,false]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
